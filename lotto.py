7d45098ea2f39ac5ba878b8bb95edfbbe041fc53
Ekaterina <pirit544@gmail.com>
2020-12-19 19:59:36 +0500

    write program structure

--- lotto.py
+++ lotto.py
@@ -1,2 +1,14 @@
 import keyboard
+import random
 
+def get_barrel():
+    print("Pressed enter")
+
+
+def main():
+    keyboard.add_hotkey('Enter', get_barrel)
+    keyboard.wait('Esc')
+
+
+if __name__ == '__main__':
+    main()
