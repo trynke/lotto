662045a1d941eab6e92823774e61cace7aa8e1ec
Ekaterina <pirit544@gmail.com>
2020-12-19 20:11:54 +0500

    write function for getting barrels number

--- lotto.py
+++ lotto.py
@@ -5,7 +5,21 @@ def get_barrel():
     print("Pressed enter")
 
 
+def get_number_of_barrels():
+    N = str(input('Enter the number of barrels. It must be an integer greater than 0 \n'))
+    if N.isdigit() and int(N) > 0:
+        return int(N)
+    else:
+        print("You entered an incorrect value. Let's try again")
+        return get_number_of_barrels()
+
+
 def main():
+    print("Random numbers generation (as in lotto game) \n")
+    N = get_number_of_barrels()
+    barrels = [i for i in range(1, N+1)]
+
+
     keyboard.add_hotkey('Enter', get_barrel)
     keyboard.wait('Esc')
 
